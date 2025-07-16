d9cc21a142596b8e0307aaf1b82696fa47cc1634
tamura shingo <tamura.shingo@gmail.com>
2025-07-17 05:09:24 +0900

    add default function

--- getcmd-test.lisp
+++ getcmd-test.lisp
@@ -34,13 +34,38 @@
                            :consume t
                            :converter ,#'parse-integer))))))
 
-
 (deftest no-args-test
-  (let ((c (getcmd '("help") *config*)))
-    (ok (eq #'help (getf c :function)))
-    (ok (equal '() (getf c :args)))
-    (ok (string= "help message" (apply (getf c :function)
-                                       (getf c :args))))))
+  (testing "without default function"
+    (let ((c (getcmd '() *config*)))
+      (ok (null (getf c :function)))
+      ;; function indicator exists, so that returns nil
+      (ok (null (getf c :function #'help)))
+      (ok (null (getf c :args)))))
+
+  (testing "with default function"
+    (let ((c (getcmd '() *config* #'help)))
+      (ok (eq (getf c :function) #'help))
+      (ok (null (getf c :args)))
+      (ok (string= "help message" (apply (getf c :function)
+                                         (getf c :args)))))))
+
+(deftest command-test
+  (testing "command exists"
+    (let ((c (getcmd '("help") *config*)))
+      (ok (eq #'help (getf c :function)))
+      (ok (equal '() (getf c :args)))
+      (ok (string= "help message" (apply (getf c :function)
+                                         (getf c :args))))))
+
+  (testing "command not exists - without default function"
+    (let ((c (getcmd '("test") *config*)))
+      (ok (null (getf c :function)))
+      (ok (equal '() (getf c :args)))))
+
+  (testing "command not exists - with default function"
+    (let ((c (getcmd '("test") *config* #'help)))
+      (ok (eq #'help (getf c :function)))
+      (ok (equal '() (getf c :args))))))
 
 
 (deftest no-option-test
